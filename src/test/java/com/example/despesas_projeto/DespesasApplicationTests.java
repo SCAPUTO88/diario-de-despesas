2f11cf838f9ede395e7c531694441f105168e422
SCAPUTO88 <sandro.caputo@gmail.com>
2025-05-30 16:52:32 -0300

    feat: adiciona logback, health e README

--- src/test/java/com/example/despesas_projeto/DespesasApplicationTests.java
+++ src/test/java/com/example/despesas_projeto/DespesasApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.despesas_projeto;
 
 import com.example.despesas_projeto.config.BaseIT;
+import com.example.despesas_projeto.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@Import(TestConfig.class)
 class DespesasApplicationTests extends BaseIT {
 
 	@Autowired
